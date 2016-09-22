201066122705c6883fb88336d9b4a07330a53423
Mike Killion <killion.kgs@gmail.com>
2016-09-22 12:16:24 -0500

    Initial working commit (basemap only).

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 require( [
   "esri/Map",
   "esri/views/MapView",
+  "esri/layers/TileLayer",
   "dojo/domReady!"
-], function(Map, MapView) {
-  var map = new Map({
-      basemap: "streets",
-      ground: "world-elevation"
-  });
+], function(Map, MapView, TileLayer) {
+  var basemapLayer = new TileLayer( {url:"http://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer", id:"Base Map"} );
+
+  var map = new Map( {
+      layers: [basemapLayer]
+  } );
+
   var view = new MapView( {
       container: "map-div",
       map: map,
       center: [-98, 38],
       zoom: 7,
   } );
+
 } );
